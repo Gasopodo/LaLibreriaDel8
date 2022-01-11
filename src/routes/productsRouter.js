b7aa61187ec34d9f47748ccb9236e4b4f4cc088b
Gasopodo <salzgaston@gmail.com>
2022-01-11 20:01:34 -0300

    añado las rutas para CRUD

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -4,6 +4,14 @@ const router = express.Router();
 const productsController = require("../controllers/productsController");
 
 router.get("/", productsController.productCart);
+router.post("/");
+router.get("/:id");
+router.put("/:id");
+router.delete("/:id");
+router.get("/:id/edit");
+
+
+router.get("/create");
 router.get("/detail", productsController.productDetail);
 router.get("/cart" , productsController.productCart);
 
